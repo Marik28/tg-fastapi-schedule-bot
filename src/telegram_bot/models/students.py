ea1f245e4fd8042976b1f25423a4d3491e4a77a6
mark <markel20002000@gmail.com>
2021-10-06 22:55:54 +0600

    добавил модель студента

---
+++ src/telegram_bot/models/students.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+
+from .groups import Group, Subgroup
+
+
+class BaseStudent(BaseModel):
+    telegram_id: int
+    subgroup: Subgroup
+
+
+class StudentCreate(BaseStudent):
+    group_name: str
+
+    class Config:
+        use_enum_values = True
+
+
+class StudentUpdate(BaseModel):
+    group_name: str
+    subgroup: Subgroup
+
+
+class Student(BaseStudent):
+    group: Group
+
+    class Config:
+        orm_mode = True
